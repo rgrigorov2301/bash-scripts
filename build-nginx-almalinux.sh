d3346deb3b3faf1873efb4566a6a2559d22c1862
rgrigorov2301 <radenko@sbi-bg.com>
2021-12-25 20:43:19 +0200

    Update build-nginx-almalinux.sh

--- build-nginx-almalinux.sh
+++ build-nginx-almalinux.sh
@@ -6,5 +6,5 @@ curl http://nginx.org/packages/rhel/8/SRPMS/nginx-1.20.2-1.el8.ngx.src.rpm > /tm
 git clone https://github.com/gabihodoroaga/nginx-ntlm-module.git /tmp/nginx-ntlm-module
 rpm -ivh nginx-1.20.2-1.el8.ngx.src.rpm
 rpm2cpio nginx-1.20.2-1.el8.ngx.src.rpm | cpio -idmv
-curl http://gate.sbi-bg.com/nginx-with-ntlm.spec > /tmp/nginx-with-ntlm.spec
+curl https://raw.githubusercontent.com/rgrigorov2301/bash-scripts/main/nginx-with-ntlm.spec > /tmp/nginx-with-ntlm.spec
 rpmbuild -bb nginx-with-ntlm.spec
